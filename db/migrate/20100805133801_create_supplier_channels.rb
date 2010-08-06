094cbfebb072481fd518c1b3e4b5be3a7098b067
Sonny Parlin <tparlin@gmail.com>
2010-08-06 10:58:10 -0700

    Added precision to cost field

--- db/migrate/20100805133801_create_supplier_channels.rb
+++ db/migrate/20100805133801_create_supplier_channels.rb
@@ -3,7 +3,7 @@ class CreateSupplierChannels < ActiveRecord::Migration
     create_table :supplier_channels do |t|
       t.column :variant_id, :integer
       t.column :supplier_id, :integer
-      t.column :cost, :decimal
+      t.column :cost, :decimal, :precision => 8, :scale => 2, :null => false
       t.column :supplier_sku, :string
     end
   end
